f1c67a73f145768c4fdc981a5a0b567ef92e1ad3
Kenneth <kencris007@gmail.com>
2025-08-24 02:06:46 -0600

    Clean up Agent and Story Visualization

--- frontend/App.js
+++ frontend/App.js
@@ -1,9 +1,4 @@
-import {
-  StyleSheet,
-  FlatList,
-  View,
-  Button,
-} from "react-native"
+import { StyleSheet, FlatList, View, Button, ScrollView } from "react-native"
 import { useState } from "react"
 import StoryForm from "./components/StoryForm"
 import StoryItem from "./components/StoryItem"
@@ -28,59 +23,62 @@ export default function App() {
      * ...stories = cada uno de los elementos en stories
      * {title: str, description: str}, {title: str, description: str}, ...
      */
-    setStories([...stories, { 
-      title, 
-      description,
-      city, 
-      type,
-      imgUri
-     }])
-    setTitle('')
-    setDescription('')
+    setStories([
+      ...stories,
+      {
+        title,
+        description,
+        city,
+        type,
+        imgUri,
+      },
+    ])
+    setTitle("")
+    setDescription("")
     setIsCreatingStory(false)
   }
 
   return (
     <View style={styles.container}>
-      {
-        // Equivalent Python is result if condition else other-result
-        isCreatingStory ? (
-          <StoryForm
-            title={title}
-            setTitle={setTitle}
-            city={city}
-            setCity={setCity}
-            description={description}
-            setDescription={setDescription}
-            handleButton={submitInfo}
-            type={type}
-            setType={setType}
-            imgUri={imgUri}
-            setImgUri={setImgUri}
-          />
-        ) : (
-          <View>
-            <FlatList
-              data={stories}
-              keyExtractor={(item, index) => index.toString()}
-              renderItem={({ item }) => (
-                <StoryItem 
-                  title={item.title} 
-                  content={item.description}
-                  city={item.city}
-                  type={item.type}
-                  imgUri={item.imgUri}  
-                />
-              )}
+        {
+          // Equivalent Python is result if condition else other-result
+          isCreatingStory ? (
+            <StoryForm
+              title={title}
+              setTitle={setTitle}
+              city={city}
+              setCity={setCity}
+              description={description}
+              setDescription={setDescription}
+              handleButton={submitInfo}
+              type={type}
+              setType={setType}
+              imgUri={imgUri}
+              setImgUri={setImgUri}
             />
-            <Button
-              title="+"
-              color="#FFC74F"
-              onPress={() => setIsCreatingStory(true)}
-            />
-          </View>
-        )
-      }
+          ) : (
+            <ScrollView>
+              <FlatList
+                data={stories}
+                keyExtractor={(item, index) => index.toString()}
+                renderItem={({ item }) => (
+                  <StoryItem
+                    title={item.title}
+                    content={item.description}
+                    city={item.city}
+                    type={item.type}
+                    imgUri={item.imgUri}
+                  />
+                )}
+              />
+              <Button
+                title="+"
+                color="#FFC74F"
+                onPress={() => setIsCreatingStory(true)}
+              />
+            </ScrollView>
+          )
+        }
     </View>
   )
 }
@@ -89,7 +87,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    justifyContent: "flex-start",
+    justifyContent: "center",
     alignItems: "center",
     margin: "1rem",
   },
